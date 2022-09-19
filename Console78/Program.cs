28adf29fa82ef5f7bffba18af67d458e170a97c3
Андрей <@DESKTOP-USER>
2022-09-19 13:34:52 +0400

    Добавьте файлы проекта.

---
+++ Console78/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[4];
+Random rnd = new Random();
+
+//создаем массив из случайных чисел от 16 до 30
+array[0] = rnd.Next(16,30);
+array[1] = rnd.Next(16,30);
+array[2] = rnd.Next(16,30);
+array[3] = rnd.Next(16,30);
+
+foreach (int a in array)
+    Console.WriteLine(a);
+
+Console.ReadLine();
